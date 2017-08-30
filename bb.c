6a970a2ffd739551e7c94662d0a782c7fed3e9c5
srisruthi1234 <srisruthi147@gmail.com>
2017-08-30 10:54:11 +0530

    Create bb.c

---
+++ bb.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+ void main()
+{
+int ivo=0;
+clrscr();
+while(ivo<=255)
+{
+printf("%d %c\n",ivo,ivo);
+ivo++;
+}
+printf("\n press any key to proced");
+getch();
+} 
